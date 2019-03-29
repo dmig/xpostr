0f5e3fda0ff72c844e4d947f6c6a8e75b59e8e05
dmig <dmitriy.geels@gmail.com>
2019-03-29 15:56:17 +0700

    use daemon rpc instead of db
    Signed-off-by: dmig <dmitriy.geels@gmail.com>

--- backend/handlers/tgchannels.py
+++ backend/handlers/tgchannels.py
@@ -1,41 +1,7 @@
-from os.path import join
-from lib.config import config
-from lib.pycnic.errors import HTTP_500, HTTPError
-from handlers.telegram import TelegramHandler, HTTP_424
+from lib.rpc_client import rpc_call
+# from lib.pycnic.errors import HTTP_500, HTTPError
+from handlers.authorized import AuthorizedHandler
 
-class TGChannels(TelegramHandler):
+class TGChannels(AuthorizedHandler):
     def get(self):
-        client = self._get_client()
-        if not client:
-            raise HTTP_424('Not authorized in telegram')
-
-        msg = None
-        try:
-            client.connect()
-
-            if not client.is_user_authorized():
-                raise HTTP_424('Not authorized in telegram')
-
-            ret = []
-            for dialog in client.get_dialogs():
-                if not dialog.is_channel or dialog.is_group:
-                    continue
-
-                photo = self._get_photo(client, dialog.entity)
-                ret.append({
-                    'id': dialog.entity.id,
-                    'title': dialog.entity.title,
-                    'photo': photo and join(config.get('telegram', 'avatars_path'), photo),
-                    'uri': dialog.entity.username
-                })
-
-            return ret
-        except HTTPError: # passthru
-            raise
-        except Exception as e:
-            self.logger.error('Telegram communication error %s', e)
-            msg = str(e)
-        finally:
-            client.disconnect()
-
-        raise HTTP_500('Internal error', msg)
+        return rpc_call('get_sources', self.user['id'], timeout=10)
