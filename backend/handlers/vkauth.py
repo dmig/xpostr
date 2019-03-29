8634c21b734a997bfc26d68d26eb32ab429c4563
dmig <dmitriy.geels@gmail.com>
2019-03-29 15:54:38 +0700

    drop requests-oauthlib, adapt to weird vk auth process
    Signed-off-by: dmig <dmitriy.geels@gmail.com>

--- backend/handlers/vkauth.py
+++ backend/handlers/vkauth.py
@@ -1,50 +1,91 @@
-from requests_oauthlib import OAuth2Session
-
-import lib.db as db
+from requests import Session
+from lib import jwt_auth
 from lib.config import config
-from lib.pycnic.errors import HTTP_400, HTTP_401, HTTP_500
+from lib.errors import VKException
+from lib.rpc_client import rpc_call
+from lib.pycnic.errors import HTTP_403, HTTP_500
 from handlers.logger import LoggerHandler
 
 class VKAuth(LoggerHandler):
     def get(self):
-        state = self.request.args.get('state')
+        code = self.request.args.get('code')
 
         # final stage
-        if state:
-            return self._final_stage(state)
-
-        params = dict(config.items('vk'))
-
-        Session = OAuth2Session(
-            params['client_id'],
-            scope='groups,offline',
-            redirect_uri=params['redirect_uri']
-        )
+        if code:
+            return self._final_stage(code)
 
         # first stage
-        authorization_url, state = Session.authorization_url(
-            'https://oauth.vk.com/authorize', state=state
-        )
-        back_url = self.request.args.get('back_url')
-        db.set_interim_state(state, {'provider': 'VK', 'back_url': back_url})
+        client_id = config.get('vk', 'client_id')
 
-        return {'auth_url': authorization_url}
+        return {
+            'auth_url': 'https://oauth.vk.com/authorize?display=page&v=5.92'
+                        '&scope=wall,photos,video,pages,docs,groups,offline'
+                        '&redirect_uri=https://api.vk.com/blank.html'
+                        '&response_type=code&revoke=1'
+                        f'&client_id={client_id}'
+        }
 
-    def _final_stage(self, state):
-        info = db.get_interim_state(state)
-        if not info:
-            raise HTTP_400('Invalid `state` parameter')
+    def _final_stage(self, code):
+        client_id = config.get('vk', 'client_id')
 
-        if 'auth_results' in info:
-            db.del_interim_state(state)
-            res = info['auth_results']
 
-            if res['ok']:
-                # self.response.set_header('Authorization', 'Bearer ' + res['token'])
-                # self.response.status_code = 303
-                # self.response.set_header('Location', info.get('back_url', '/'))
+        if not code:
+            self.logger.info('Error response from VK: %s', self.request.args)
 
-                return {'token': 'Bearer ' + res['token']}
+            raise HTTP_403(
+                self.request.args.get('error_description'),
+                self.request.args.get('error_reason', self.request.args.get('error'))
+            )
 
-            raise HTTP_401(res['message'], res['code'])
+        client_id = config.get('vk', 'client_id')
+        client_secret = config.get('vk', 'client_secret')
+        vksession = Session()
+        try:
+            resp = vksession.get(
+                'https://oauth.vk.com/access_token?'
+                'redirect_uri=https://api.vk.com/blank.html'
+                f'&client_id={client_id}&client_secret={client_secret}&code={code}'
+            )
+            resp.raise_for_status()
+
+            auth = resp.json()
+            if not auth.get('access_token'):
+                raise VKException(auth)
+
+            resp = vksession.get(
+                'https://api.vk.com/method/users.get',
+                params={
+                    'v': '5.92',
+                    'user_ids': auth['user_id'],
+                    'fields': 'photo_100',
+                    'access_token': auth['access_token']
+                }
+            )
+            resp.raise_for_status()
+            user = resp.json()
+            if user.get('error'):
+                raise VKException(user)
+
+            user = user.get('response', {})
+            user = user[0] if user else {}
+
+            res = rpc_call('set_vk_user', user)
+
+            token = jwt_auth.create_token({
+                'id': auth['user_id'],
+                'aud': 'authorized'
+            })
+
+            self.logger.info('Issued JWT: %s', token)
+
+            return {'ok': True, 'token': token.decode("utf-8")}
+        except VKException as e:
+            self.logger.warning('Error authorizing user on VK: %s', e)
+
+            raise HTTP_403(e.message, e.code)
+        except Exception as e:
+            self.logger.exception('Error authorizing user on VK: %s', e)
+        finally:
+            vksession.close()
+
         raise HTTP_500('Internal error')
