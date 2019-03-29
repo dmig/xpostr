bf32f2c2a858265af2dffcafc1a511c9def079c5
dmig <dmitriy.geels@gmail.com>
2019-03-29 15:52:51 +0700

    app error classes
    Signed-off-by: dmig <dmitriy.geels@gmail.com>

---
+++ backend/lib/errors.py
@@ -0,0 +1,13 @@
+class VKException(Exception):
+    def __init__(self, error):
+        self.message = error.get('error_msg', error.get('error_description'))
+        self.code = error.get('error_code', error.get('error'))
+        self.extra = error.get('request_params', error.get('error_reason'))
+        super().__init__(self.message, self.code, self.extra)
+
+# pylint: disable=multiple-statements
+class XPostrException(Exception): pass
+class MissingParam(Exception): pass
+class UnknownUser(XPostrException): pass
+class UnauthorizedException(XPostrException): pass
+class StartupException(XPostrException): pass
