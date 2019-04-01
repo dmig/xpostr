69713a598bd6e1642fddbb155fab1c9f75816779
dmig <dmitriy.geels@gmail.com>
2019-04-01 17:57:17 +0700

    bugfix, add log message
    Signed-off-by: dmig <dmitriy.geels@gmail.com>

--- backend/lib/daemon/handlers/common.py
+++ backend/lib/daemon/handlers/common.py
@@ -1,5 +1,8 @@
+import logging
 from lib.daemon import context
 from lib.daemon.core import repost_message, get_client
+
+_logger = logging.getLogger(__name__)
 
 def handle_ping():
     return {'clients': len(context.accounts)}
@@ -12,7 +15,8 @@ async def handle_repost_message(vk_user_id: int, vk_group_id: int, tg_channel_id
 
     client = await get_client(vk_user_id)
     await client.get_dialogs()
-    message = await client.get_messages(client.get_entity(tg_channel_id), ids=msg_id)
+    message = await client.get_messages(await client.get_entity(tg_channel_id), ids=msg_id)
+    _logger.info('Found message: %s', message)
 
     await repost_message(vk_user_id, vk_group_id, message)
     return True
