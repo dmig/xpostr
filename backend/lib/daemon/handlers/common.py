cb1ac24b5ccb387fe203a326407235ea51b706ba
dmig <dmitriy.geels@gmail.com>
2019-04-01 16:36:37 +0700

    new rpc handler
    Signed-off-by: dmig <dmitriy.geels@gmail.com>

--- backend/lib/daemon/handlers/common.py
+++ backend/lib/daemon/handlers/common.py
@@ -1,4 +1,18 @@
 from lib.daemon import context
+from lib.daemon.core import repost_message, get_client
 
 def handle_ping():
     return {'clients': len(context.accounts)}
+
+async def handle_repost_message(vk_user_id: int, vk_group_id: int, tg_channel_id: int, msg_id: int):
+    vk_user_id = int(vk_user_id)
+    vk_group_id = int(vk_group_id)
+    tg_channel_id = int(tg_channel_id)
+    msg_id = int(msg_id)
+
+    client = await get_client(vk_user_id)
+    await client.get_dialogs()
+    message = await client.get_messages(client.get_entity(tg_channel_id), ids=msg_id)
+
+    await repost_message(vk_user_id, vk_group_id, message)
+    return True
