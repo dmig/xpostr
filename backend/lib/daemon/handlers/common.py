8238e9f56ff82a614c78039ae90f714b1b8d5cb7
dmig <dmitriy.geels@gmail.com>
2019-04-02 09:48:43 +0700

    minor fix

--- backend/lib/daemon/handlers/common.py
+++ backend/lib/daemon/handlers/common.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from lib.daemon import context
-from lib.daemon.core import repost_message, get_client
+from lib.daemon.core import repost_message, get_client, catch_task_exception
 
 _logger = logging.getLogger(__name__)
 
@@ -20,5 +20,5 @@ async def handle_repost_message(vk_user_id: int, vk_group_id: int, tg_channel_id
     _logger.info('Found message: %s', message)
 
     asyncio.ensure_future(repost_message(vk_user_id, vk_group_id, message))\
-        .add_done_callback(lambda _: _logger.info('Repost done...'))
+        .add_done_callback(lambda fut: catch_task_exception(fut) or _logger.info('Repost done...'))
     return True
