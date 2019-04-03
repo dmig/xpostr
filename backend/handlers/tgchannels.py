5838250e6660e649a48af88bd245136bc9e0c490
dmig <dmitriy.geels@gmail.com>
2019-04-03 13:37:10 +0700

    increase timeouts
    Signed-off-by: dmig <dmitriy.geels@gmail.com>

--- backend/handlers/tgchannels.py
+++ backend/handlers/tgchannels.py
@@ -4,6 +4,6 @@ from handlers.telegram import TelegramHandler
 class TGChannels(TelegramHandler):
 
     def get(self):
-        res = list(map(self._set_photo_path, rpc_call('get_sources', self.user['id'], timeout=10)))
+        res = list(map(self._set_photo_path, rpc_call('get_sources', self.user['id'], timeout=30)))
         res.sort(key=lambda item: item['title'])
         return res
